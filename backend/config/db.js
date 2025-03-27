398d3356e5fa864f6810d9d84e9aa6a350218fad
Keipher-Mada <k.dubber17@gmail.com>
2025-03-27 12:06:36 +0000

    added gitignore for backend

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-  await mongoose
-    .connect('mongodb+srv://kdubber17:14751367133@cluster0.vlr6fsq.mongodb.net/Gasha')
-    .then(() => console.log('DB Connected'));
-};
