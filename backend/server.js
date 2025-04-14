60c1124590b011c5fb11cba78a902ecc5c219c52
Keipher-Mada <k.dubber17@gmail.com>
2025-04-14 20:01:49 +0100

    add JWT user autiethcation function v.1

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import gashaRouter from './routes/gashaRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config';
 
 //app config
 const app = express();
@@ -17,6 +19,7 @@ connectDB();
 //api end points
 app.use('/api/gasha', gashaRouter);
 app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
   res.send('API Working');
